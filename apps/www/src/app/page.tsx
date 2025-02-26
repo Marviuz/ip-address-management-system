5e00c650533d3e784e31bd42f8545322cd1993e5
Marviuz <marviuz.gaming@gmail.com>
2025-02-26 22:18:53 +0800

    feat(www): initial authentication

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <div>
+      Home page
+      <a href="http://localhost:8000/auth/google">login with google</a>
+    </div>
+  );
 }
