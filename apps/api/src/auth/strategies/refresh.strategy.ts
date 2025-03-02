04befe76abe23207af6e513aa85a147088d176d9
Marviuz <marviuz.gaming@gmail.com>
2025-03-02 15:13:48 +0800

    refactor: check if refresh token match token from db

--- apps/api/src/auth/strategies/refresh.strategy.ts
+++ apps/api/src/auth/strategies/refresh.strategy.ts
@@ -1,5 +1,6 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { type Request } from 'express';
 import { env } from 'src/env';
 import { type TokenPayload } from 'src/types/user';
 import { type AuthService } from '../auth.service';
@@ -10,12 +11,21 @@ export class RefreshStrategy extends PassportStrategy(Strategy, 'refresh') {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: env.AUTH_SECRET,
       ignoreExpiration: false,
+      passReqToCallback: true,
     });
   }
 
-  async validate(payload: TokenPayload) {
+  async validate(req: Request, payload: TokenPayload) {
     const userPublicId = payload.sub;
-    const userId = await this.authService.validateRefreshToken(userPublicId);
-    return userId;
+
+    const refreshToken = this.authService.extractBearerTokenFromAuthHeader(
+      req.headers.authorization,
+    );
+    const user = await this.authService.validateRefreshToken(
+      userPublicId,
+      refreshToken,
+    );
+
+    return user;
   }
 }
