00ef2a851f58d884cb246e2783f050bcb9ee097d
Marviuz <marviuz.gaming@gmail.com>
2025-03-09 09:45:25 +0800

    chore: update deps and remove nextjs workspace

--- apps/www.bak/src/features/network-address-data-table/utils/get-network-address-type.ts
+++
@@ -1,8 +0,0 @@
-import { isIP, isMACAddress } from 'validator';
-
-export function getNetworkAddressType(value: string) {
-  if (isIP(value, '4')) return 'IPv4';
-  if (isIP(value, '6')) return 'IPv6';
-  if (isMACAddress(value)) return 'MAC';
-  return null;
-}
--- apps/www.bak/src/lib/services/get-authed-user.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-
-import { userSchema } from '@ip-address-management-system/shared';
-import { api } from '../api-client';
-
-export async function getAuthedUser() {
-  return api.private((client) => client.get('users/me').json(), userSchema);
-}
