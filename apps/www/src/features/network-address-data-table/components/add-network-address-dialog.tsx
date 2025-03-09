78126e33ad0da7492a084d35973227a6ed5565ae
Marviuz <marviuz.gaming@gmail.com>
2025-03-09 18:20:57 +0800

    feat: cleanup dialog on network id add

--- apps/www/src/features/network-address-data-table/components/add-network-address-dialog.tsx
+++ apps/www/src/features/network-address-data-table/components/add-network-address-dialog.tsx
@@ -1,6 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Plus } from 'lucide-react';
-import { type FC } from 'react';
+import { useState, type FC } from 'react';
 import { addNetworkAddress } from '../lib/services/add-network-address';
 import { NetworkAddressForm } from './network-address-form';
 import {
@@ -15,16 +15,18 @@ import { Button } from '@/components/common/button';
 import { queries } from '@/lib/queries';
 
 export const AddNetworkAddressDialog: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const queryClient = useQueryClient();
   const { mutate } = useMutation({
     mutationFn: addNetworkAddress,
     onSuccess: async () => {
       await queryClient.refetchQueries(queries.networkAddress.all);
+      setIsOpen(false);
     },
   });
 
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild className="ml-auto flex">
         <Button>
           <Plus />
