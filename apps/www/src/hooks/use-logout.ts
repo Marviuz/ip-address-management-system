2a650ebbfa9423ff545a45229725d4d435dec343
Marviuz <marviuz.gaming@gmail.com>
2025-03-09 15:10:28 +0800

    fix: unauthorized logout

--- apps/www/src/hooks/use-logout.ts
+++ apps/www/src/hooks/use-logout.ts
@@ -1,24 +1,22 @@
 import { useRouter } from '@tanstack/react-router';
 import { useMutation } from '@tanstack/react-query';
 import { useAuth } from '@/contexts/auth-context/use-auth';
-import { logout } from '@/lib/services/sign-out';
+import { logout } from '@/lib/services/logout';
 
 export function useLogout() {
   const auth = useAuth();
-  const { mutate } = useMutation({
-    mutationFn: logout,
-  });
-
   const router = useRouter();
 
-  const logoutMutation = async () => {
-    mutate();
-    auth?.setAccessToken(null);
-    await router.navigate({
-      to: '/',
-      replace: true,
-    });
-  };
+  const { mutate } = useMutation({
+    mutationFn: logout,
+    onSuccess: async () => {
+      auth?.setAccessToken(null);
+      await router.navigate({
+        to: '/',
+        replace: true,
+      });
+    },
+  });
 
-  return { logout: logoutMutation };
+  return { logout: mutate };
 }
--- apps/www/src/routes/_authenticated.tsx
+++ apps/www/src/routes/_authenticated.tsx
@@ -1,8 +1,22 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router';
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router';
 import { Button } from '@/components/common/button';
 import { useLogout } from '@/hooks/use-logout';
+import { refreshToken } from '@/lib/services/refresh-token';
 
 export const Route = createFileRoute('/_authenticated')({
+  beforeLoad: async ({ context }) => {
+    if (!context.auth?.accessToken) {
+      const newTokens = await refreshToken();
+      if (newTokens.success) {
+        context.auth?.setAccessToken(newTokens.data.access_token);
+        return;
+      }
+
+      return redirect({
+        to: '/',
+      });
+    }
+  },
   component: AuthenticatedLayout,
 });
 
