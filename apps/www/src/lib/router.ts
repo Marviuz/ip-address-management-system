a57383177e612f0cddb23aa253b64d1faafce8e3
Marviuz <marviuz.gaming@gmail.com>
2025-03-11 22:33:05 +0800

    feat: route change indicator

--- apps/www/src/lib/router.ts
+++ apps/www/src/lib/router.ts
@@ -1,4 +1,5 @@
 import { createRouter } from '@tanstack/react-router';
+import nProgress from 'nprogress';
 import { routeTree } from '@/routeTree.gen';
 import { queryClient } from '@/contexts/query-context/core';
 
@@ -9,6 +10,10 @@ export const router = createRouter({
     queryClient,
   },
 });
+
+nProgress.configure({ showSpinner: false });
+router.subscribe('onBeforeLoad', () => nProgress.start());
+router.subscribe('onResolved', () => nProgress.done());
 
 declare module '@tanstack/react-router' {
   // eslint-disable-next-line @typescript-eslint/consistent-type-definitions -- module augmentation
