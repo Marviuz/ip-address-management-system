92bce4d8343020fe8d86cfb8a408b2366046d540
Marviuz <marviuz.gaming@gmail.com>
2025-03-11 22:12:54 +0800

    refactor: only allow ip addresses

--- apps/www/src/features/network-address-data-table/lib/schema.ts
+++ apps/www/src/features/network-address-data-table/lib/schema.ts
@@ -1,14 +1,10 @@
 import { z } from 'zod';
 import isIP from 'validator/es/lib/isIP';
-import isMACAddress from 'validator/es/lib/isMACAddress';
 
 export const networkAddressFormSchema = z.object({
   networkAddress: z
     .string()
-    .refine(
-      (value) => isIP(value) || isMACAddress(value),
-      'Invalid Network Address',
-    ),
+    .refine((value) => isIP(value), 'Invalid Network Address'),
   label: z.string().min(1, 'This field is required'),
   comments: z.string(),
 });
